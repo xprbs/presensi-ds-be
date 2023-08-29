84196081b489926aff60a3e22e5f1e953ade0f29
Pandu Prabu <xprbs.me@gmail.com>
2023-08-29 07:35:36 +0700

    feat: move reset-password route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\PerimeterController;
 
 Route::get('login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('login', [AuthController::class, 'authenticate'])->name('postLogin');
+Route::get('reset-password/{token}', [AuthController::class, 'verifyToken'])->name('reset-password');
 
 Route::middleware('isAuth')->group(function () {
     Route::get('/', function () {
@@ -26,7 +27,6 @@ Route::middleware('isAuth')->group(function () {
     Route::get('presence', [PresenceController::class, 'index'])->name('presence.index');
     Route::get('presence/get-student/{classroom}', [PresenceController::class, 'getStudent'])->name('presence.getStudent');
     Route::get('presence/student', [PresenceController::class, 'perStudent'])->name('presence.student');
-    Route::get('reset-password/{token}', [AuthController::class, 'verifyToken'])->name('reset-password');
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 });
